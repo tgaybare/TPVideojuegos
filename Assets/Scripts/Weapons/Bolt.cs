a96d80e81fc42cd8c7f7a8628cf1d00562d09a2a
tgaybare <tomigayba02yt@gmail.com>
2024-04-14 18:42:18 -0300

    feat: arrow moving when shot

---
+++ Assets/Scripts/Weapons/Bolt.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace Weapons
+{
+    public class Bolt : MonoBehaviour, IProjectile
+    {
+        public int Damage { get; }
+        public float Speed { get; set; }
+        public float LifeTime { get; }
+
+        private Vector3 _direction;
+
+        private void Start()
+        {
+            _direction = transform.forward;
+            Speed = 10;
+        }
+
+        private void Update()
+        {
+            transform.position +=  Time.deltaTime * Speed * _direction ;
+        }
+
+
+        public void Travel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCollisionEnter(Collision collision)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
