0568044219ab0aaa52d8d94d026daa119a0eecec
Julian Sasso <jsasso@itba.edu.ar>
2024-04-27 18:21:04 -0300

    Implement life time for Bolt script

--- Assets/Scripts/Weapons/Bolt.cs
+++ Assets/Scripts/Weapons/Bolt.cs
@@ -1,36 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Weapons
 {
     public class Bolt : MonoBehaviour, IProjectile
     {
-        public int Damage { get; }
-        public float Speed { get; set; }
-        public float LifeTime { get; }
+
+        [SerializeField] private int _damage = 10;
+        [SerializeField] private float _speed = 50;
+        [SerializeField] private float _lifetime = 5;
+        [SerializeField] private List<int> _layerMasks;
+
+        public int Damage => _damage;
+        public float Speed => _speed;
+        public float LifeTime => _lifetime;
 
         private Vector3 _direction;
 
         private void Start()
         {
-            _direction = transform.forward;
-            Speed = 10;
+           // _direction = transform.forward;
         }
 
         private void Update()
         {
-            transform.position +=  Time.deltaTime * Speed * _direction ;
+            //transform.position +=  Time.deltaTime * Speed * _direction ;
+            Travel();
+
+            _lifetime -= Time.deltaTime;
+            if (_lifetime <= 0) Destroy(this.gameObject);
         }
 
 
         public void Travel()
         {
-            throw new NotImplementedException();
+            transform.position += transform.forward * Time.deltaTime * Speed;
         }
 
         public void OnCollisionEnter(Collision collision)
         {
-            throw new System.NotImplementedException();
+            if (_layerMasks.Contains(collision.gameObject.layer))
+            {
+                IDamagable damagable = collision.gameObject.GetComponent<IDamagable>();
+                damagable?.TakeDamage(Damage);
+
+                Destroy(this.gameObject);
+            }
         }
     }
 }
