3556217c57c1ba3ce3a4eabe0eb0ada0452d7e3b
tgaybare <tomigayba02yt@gmail.com>
2024-05-04 18:09:36 -0300

    ft: new Enemy type. Plague doctor (distance enemy)

---
+++ Assets/Scripts/Enemy/EnemyAIManager.cs
@@ -0,0 +1,63 @@
+﻿using Commands;
+using Strategy.Strategy___Movement;
+using Strategy.Strategy___Weapon;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class EnemyAIManager : MonoBehaviour
+    {
+        [SerializeField] private IWeapon _weapon;
+        
+        public EnemyStats Stats => stats;
+        [SerializeField] private EnemyStats stats;
+        
+        [SerializeField] private GameObject player;
+        private float AttackRange => stats.AttackRange;
+        private float AttackRate => stats.AttackRate;
+    
+        private CmdMovement _cmdMoveDirection;
+        private CmdAttack _cmdAttack;
+        
+        private double timeSinceLastAttack = 0;
+        
+        private void Awake()
+        {
+            // Movement directions
+            Vector3 forward = new Vector3(0, 0, 1);
+
+            _weapon = GetComponent<IWeapon>();
+
+            // Movement Commands
+            _cmdMoveDirection = new CmdMovement(forward, GetComponent<IMoveable>());
+        
+            // Attack
+            _cmdAttack = new CmdAttack(_weapon);
+        }
+        
+        void Update()
+        {
+            IMoveable enemy = GetComponent<IMoveable>();
+            Ray playerDirection = new Ray(transform.position, player.transform.position - transform.position);
+            enemy.RotateTowards(playerDirection);
+        
+            //Attack
+            if (Vector3.Distance(transform.position, player.transform.position) < AttackRange)
+            {
+                if (timeSinceLastAttack > AttackRate)
+                {
+                    _cmdAttack.Do();
+                    timeSinceLastAttack = 0;
+                }
+                timeSinceLastAttack += Time.deltaTime;
+            }
+            else
+            {
+                _cmdMoveDirection.ChangeDirection(playerDirection.direction);
+                _cmdMoveDirection.Do();
+                
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Weapons/MagicStaff.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Weapons
+{
+    public class MagicStaff : DistanceWeapon
+    {
+        
+        public override void Attack()
+        {
+            Vector3 staffBarrelPosition = GameObject.FindWithTag("MagicStaff").GetComponent<Collider>().transform.position;
+            Vector3 position = staffBarrelPosition;
+            Instantiate(
+                ProjectilePrefab, 
+                position, 
+                transform.rotation);
+        }
+        
+    }
+}
