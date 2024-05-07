6c1221a9cec79f1139702fd7bff50edd5189a6c7
tgaybare <tomigayba02yt@gmail.com>
2024-05-07 16:51:31 -0300

    ft: Dodge implemented

---
+++ Assets/Scripts/Commands/CmdDodge.cs
@@ -0,0 +1,21 @@
+﻿using Strategy.Strategy___Movement;
+
+namespace Commands
+{
+    public class CmdDodge : ICommand
+    {
+        private IMoveable _moveable;
+        private int _duration;
+
+        public CmdDodge(IMoveable moveable, int duration)
+        {
+            _moveable = moveable;
+            _duration = duration;
+        }
+
+        public void Do()
+        {
+            _moveable.Dodge(_duration);
+        }
+    }
+}
