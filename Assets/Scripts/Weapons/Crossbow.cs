23699cba049505e24bfee0132830930ab2f27ecd
tgaybare <tomigayba02yt@gmail.com>
2024-05-21 17:26:35 -0300

    ft: added crossbow firing sound

--- Assets/Scripts/Weapons/Crossbow.cs
+++ Assets/Scripts/Weapons/Crossbow.cs
@@ -1,14 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Sound;
 using UnityEngine;
 
 public class Crossbow : DistanceWeapon
 {
     private float _reloadTime = 500f;
+
+    private void Awake()
+    {
+        _soundPlayer = gameObject.GetComponentInChildren<FixedSoundPlayer>();
+    }
+
+    private FixedSoundPlayer _soundPlayer;
     public override void Attack()
     {
         GameObject crossbow = GameObject.FindWithTag("Crossbow");
         Vector3 position = new Vector3(transform.position.x, crossbow.transform.position.y, transform.position.z);
+        _soundPlayer.Play();
 
         Instantiate(
             ProjectilePrefab, 
