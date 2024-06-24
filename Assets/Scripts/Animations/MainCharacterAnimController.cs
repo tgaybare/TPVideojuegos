3b72b0d7466c33186414345e92fb1433176ae0db
tgaybare <tomigayba02yt@gmail.com>
2024-06-24 19:09:51 -0300

    ft: main character death animation

---
+++ Assets/Scripts/Animations/MainCharacterAnimController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Animations
+{
+    public class MainCharacterAnimController : MonoBehaviour
+    {
+        private Animator _animator;
+
+        private void OnEnable()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        public void Death()
+        {
+            _animator.SetTrigger("Death");
+        }
+    }
+}
