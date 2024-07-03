0221fc920b70cc8dc56f4a625d3a4414454feb21
Julian Sasso <jsasso@itba.edu.ar>
2024-07-03 13:23:50 -0300

    Implement UpgradeHolder class

---
+++ Assets/Scripts/UI/UpgradeHolder.cs
@@ -0,0 +1,59 @@
+﻿using Assets.Scripts.Upgrades;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class UpgradeHolder : MonoBehaviour
+{
+    [SerializeField] private List<UpgradeIcon> _upgradeIcons = new();
+    FixedSizedQueue<IAppliableUpgrade> _shownUpgrades;
+
+    private int _maxUpgradeCount => _upgradeIcons.Count;
+
+    #region SINGLETON
+    public static UpgradeHolder instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    #endregion SINGLETON
+
+    void Start()
+    {
+        _shownUpgrades = new(_maxUpgradeCount);
+    }
+
+    public void AddUpgrade(IAppliableUpgrade upgrade)
+    {
+        _shownUpgrades.Enqueue(upgrade);
+
+        if(_shownUpgrades.Count == _maxUpgradeCount)
+        {
+            shiftIcons();
+        } else 
+        {
+            _upgradeIcons[_maxUpgradeCount - _shownUpgrades.Count].UpdateUpgrade(upgrade.GetUpgradeID(), upgrade.GetSprite());
+        }
+    }
+
+    // Update the icons to show the new upgrades, shifting the icons to the right
+    // and removing the oldest one
+    private void shiftIcons() {
+        IAppliableUpgrade[] toShow = _shownUpgrades.ToArray();
+        
+        for (int i = 0; i < _maxUpgradeCount; i++)
+        {
+            _upgradeIcons[i].UpdateUpgrade(toShow[i].GetUpgradeID(), toShow[i].GetSprite());
+        }
+    }
+        
+}
---
+++ Assets/Scripts/Utils/FixedSizeQueue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedSizedQueue<T> : Queue<T>
+{
+    public int Size { get; private set; }
+
+    public FixedSizedQueue(int size)
+    {
+        Size = size;
+    }
+
+    public new void Enqueue(T obj)
+    {
+        base.Enqueue(obj);
+
+        if (base.Count > Size)
+        {
+            base.Dequeue();
+        }
+        
+    }
+}
