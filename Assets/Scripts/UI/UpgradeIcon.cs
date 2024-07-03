f4bb00ece77dc44f686cae5600c096ca7b1d2789
Julian Sasso <jsasso@itba.edu.ar>
2024-07-03 12:27:23 -0300

    Implement UpgradeIcon class

---
+++ Assets/Scripts/UI/UpgradeIcon.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Scripts.Upgrades;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UpgradeIcon : MonoBehaviour
+{
+    [SerializeField] private Image _image;
+    [SerializeField] private UpgradeID _upgradeID = UpgradeID.NONE;
+
+    private void Awake()
+    {
+        _image = GetComponentInChildren<Image>();
+    }
+
+    public void UpdateUpgrade(UpgradeID upgradeID, Sprite upgradeSprite)
+    {
+        _image.sprite = upgradeSprite;
+        _upgradeID = upgradeID;
+    }
+}
